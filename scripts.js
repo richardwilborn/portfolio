a0e438b68c2c39b76d65aafe3fde4fff50d9faae
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2023-02-04 11:57:28 -0600

    moved js scripts to seperate file

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("download-button").addEventListener("click", function() {
+  window.open("https://drive.google.com/file/d/1OTvON349y9UnPutWSl2lD8wI5T6aFa40/view?usp=sharing");
+});
